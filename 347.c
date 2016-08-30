e461bd775c12c0e23b17148b75affd6acb3bdd53
kurisusnowdeng <kurisusnowdeng@users.noreply.github.com>
2016-08-30 18:19:40 +0800

    Update 347.c

--- 347.c
+++ 347.c
@@ -3,12 +3,12 @@
 
 #define P 999983
 
-typedef struct map {
+typedef struct _map {
 	int value;
 	int cnt;
 };
 
-void find(int* hash, struct map *arr, int *n, int x, int q) {
+void find(int* hash, struct _map *arr, int *n, int x, int q) {
 	int key = x % q;
 	key = (key * key) % q;
 	while (hash[key] >= 0 && arr[hash[key]].value != x) {
@@ -25,15 +25,15 @@ void find(int* hash, struct map *arr, int *n, int x, int q) {
 	}
 }
 
-void sort(struct map *arr, int k, int low, int high) {
+void sort(struct _map *arr, int k, int low, int high) {
 	if (high < k || low > k) return;
 	int pivot = arr[(low + high) / 2].cnt;
 	int i = low;
 	int j = high;
-	struct map temp;
+	struct _map temp;
 	while (i <= j) {
-		while (arr[i].cnt >= pivot) ++i;
-		while (arr[j].cnt <= pivot) --j;
+		while (arr[i].cnt > pivot) ++i;
+		while (arr[j].cnt < pivot) --j;
 		if (i <= j) {
 			temp.value = arr[i].value;
 			temp.cnt = arr[i].cnt;
@@ -53,21 +53,23 @@ int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
 	int *ans = (int*)malloc(sizeof(int) * (*returnSize));
 	memset(ans, 0, sizeof(int) * (*returnSize));
 	int hashlen = P;
-	if (hashlen < numsSize) {
-		hashlen = (numsSize << 1) - 1;
+	if (hashlen < nums.size()) {
+	    hashlen = (nums.size() << 1) - 1;
 	}
 	int *hash = (int*)malloc(sizeof(int) * hashlen);
-	memset(hash, 0xff, sizeof(int) * hashlen);
-	int n = 0;
-	struct map *arr = (struct map *)malloc(sizeof(struct map) * numsSize);
-	memset(arr, 0, sizeof(struct map) * numsSize);
-	for (int i = 0; i < numsSize; i++) {
-		find(hash, arr, &n, nums[i], hashlen);
-	}
-	sort(arr, k, 0, n - 1);
+    memset(hash, 0xff, sizeof(int) * hashlen);
+    int n = 0;
+    struct _map *arr = (struct _map *)malloc(sizeof(struct _map) * nums.size());
+    memset(arr, 0, sizeof(struct _map) * nums.size());
+    for (int i = 0; i < nums.size(); i++) {
+    	find(hash, arr, &n, nums[i], hashlen);
+    }
+    free(hash);
+	sort(arr, k - 1, 0, n - 1);
 	for (int i = 0; i < k; i++) {
 		ans[i] = arr[i].value;
 	}
+	free(arr);
 	return ans;
 }
 
