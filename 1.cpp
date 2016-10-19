e16ab9eb5e461f086623164ee525f13d0081312d
kurisusnowdeng <kurisusnowdeng@users.noreply.github.com>
2016-10-19 23:06:30 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,50 @@
 class Solution {
+private:
+	int *hash;
+	int *idx;
+	int q;
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        ans.clear();
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
-            }
-        }
-        return ans;
-    }
+	void push(int x, int i) {
+		int key = ((x % q) * (x % q)) % q;
+		while (idx[key] >= 0) {
+			key = (key + 1) % q;
+		}
+		hash[key] = x;
+		idx[key] = i;
+	}
+
+	int find(int x) {
+		int key = ((x % q) * (x % q)) % q;
+		while (hash[key] != x) {
+			if (idx[key] < 0) {
+				return -1;
+			}
+			key = (key + 1) % q;
+		}
+		return idx[key];
+	}
+
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> res;
+		res.clear();
+		q = (nums.size() << 1) + 1;
+		hash = new int[q];
+		idx = new int[q];
+		memset(hash, 0xff, sizeof(int) * q);
+		memset(idx, 0xff, sizeof(int) * q);
+		for (int i = 0; i < nums.size(); i++) {
+			push(nums[i], i);
+		}
+		for (int i = 0; i < nums.size(); i++) {
+			int j = find(target - nums[i]);
+			if (j >= 0 && j != i) {
+				res.push_back(i);
+				res.push_back(j);
+				break;
+			}
+		}
+		delete[] hash;
+		delete[] idx;
+		return res;
+	}
 };
